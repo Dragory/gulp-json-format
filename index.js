d912f3293f88ce38855f436f19d6b45a1bb1e5e9
Igor Lyubimov <gboslee@gmail.com>
2015-10-14 16:01:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var streamMap = require('map-stream'),
-	through = require('through');
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through');
+var streamMap = require('map-stream');
 
 module.exports = function(spaces, endWithNewLine) {
-	return streamMap(function(file, cb) {
-		var stream = this;
-
-		var replacer = through(function(data) {
-			var formatted = JSON.stringify(JSON.parse(data.toString()), null, spaces) + (endWithNewLine ? '\n' : '');
-
-			if (true || file.isBuffer()) {
-				file.contents = new Buffer(formatted);
-				cb(null, file);
-			} else {
-				file.contents = through();
-				cb(null, file);
-
-				file.contents.write(new Buffer(formatted));
-				file.contents.end();
-			}
-		});
-
-		file.pipe(replacer);
-	});
-};
+  return streamMap(function(file, cb) {
+    var stream = this;
+    var replacer = through(function(data) {
+      try {
+        var formatted = JSON.stringify(JSON.parse(data.toString()), null, spaces) + (endWithNewLine ? '\n' : '');
+        if (true || file.isBuffer()) {
+          file.contents = new Buffer(formatted);
+        } else {
+          file.contents = through();
+          file.contents.write(new Buffer(formatted));
+          file.contents.end();
+        }
+        gutil.log(gutil.colors.green((path.basename(file.history[0]))), gutil.colors.blue('ok'));
+      } catch (error) {
+        gutil.log(gutil.colors.green((path.basename(file.history[0]))), gutil.colors.red('error'));
+      }
+      cb(null, file);
+    });
+    file.pipe(replacer);
+  });
+}
